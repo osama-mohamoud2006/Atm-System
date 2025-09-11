766a9f94ff8911ea8e5d26177aa40c982d715e9c
osama mohamoud <wwwhdosama@gmail.com>
2025-09-11 19:29:45 +0300

    Auto commit (Project 2): 2025-09-11 19:29:44

--- ATM System/ATM System.cpp
+++ ATM System/ATM System.cpp
@@ -26,6 +26,7 @@ void login();
 const string path = "local db.text"; // Clients
 const string delmi = "#//#";
 
+// things to use in menu option 
 void back_to_menu(string TextAppearWhenYouBack = "press any key to back to main menu !") {
 	screen_color(black);
 	cout << "\033[1;31m";
@@ -34,10 +35,33 @@ void back_to_menu(string TextAppearWhenYouBack = "press any key to back to main 
 	system("pause>0");
 
 }
-
 void print_menu_option(string option_name) {
 	cout << "\n_________________________________________________\n\n\n";
 	cout << "\t" << option_name << "\n";
 	cout << "\n_________________________________________________\n";
 }
 
+
+
+
+
+
+
+
+
+void main_menu() {
+	system("cls");
+	cout << setw(5) << "\tWelcome to Atm System! ";
+	cout << "\n_____________________________________________________\n\n";
+	cout << setw(5) << "please enter the number of the option you want: \n" << endl;
+
+	cout << setw(4) << "\t[1] Quick Withdraw." << endl;
+	cout << setw(4) << "\t[2] Normal Withdraw." << endl;
+	cout << setw(4) << "\t[3] Deposit." << endl;
+	cout << setw(4) << "\t[4] Check Balance." << endl;
+	cout << setw(4) << "\t[5] logout." << endl;
+	cout << "\n_____________________________________________________\n\n";
+	cout << "Please enter the option you want: ";
+
+
+}
