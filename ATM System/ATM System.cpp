67ce5b0cedf1882621af8c8402a7474de8f224f5
osama mohamoud <wwwhdosama@gmail.com>
2025-09-11 19:26:21 +0300

    Auto commit (Project 2): 2025-09-11 19:26:21

--- ATM System/ATM System.cpp
+++ ATM System/ATM System.cpp
@@ -9,10 +9,35 @@
 using namespace AllStuff;
 using namespace std;
 
-struct StUser { int account_balance = 0; int pin = 0; int account_number = 0; };
+struct StUser {
+string account_number = " "; // this is the main key which is very importat to manage data 
+string pin = " ";
+string name = " ";
+string phone = " ";
+int account_balance = 0;
+
+};
 
 struct StUser CurrentUser;
 
 enum enMainMenuOptions{QuicWithdraw=1 , NormalWithdraw=2 ,Dposit=3 , CheckBalance=4 ,Logout=5};
 
- 
+void login();
+const string path = "local db.text"; // Clients
+const string delmi = "#//#";
+
+void back_to_menu(string TextAppearWhenYouBack = "press any key to back to main menu !") {
+	screen_color(black);
+	cout << "\033[1;31m";
+	cout << "\n " << TextAppearWhenYouBack << endl;
+	cout << "\033[0m";
+	system("pause>0");
+
+}
+
+void print_menu_option(string option_name) {
+	cout << "\n_________________________________________________\n\n\n";
+	cout << "\t" << option_name << "\n";
+	cout << "\n_________________________________________________\n";
+}
+
