c8cbc71e6b41ea34e8169597db33bcc003e5ab41
osama mohamoud <wwwhdosama@gmail.com>
2025-09-13 17:36:25 +0300

    Auto commit (Project 2): 2025-09-13 17:36:25

--- Resolve with another way/Resolve with another way.cpp
+++ Resolve with another way/Resolve with another way.cpp
@@ -21,7 +21,7 @@ enum enMainMenueOptions {
     eQucikWithdraw = 1, eNormalWithDraw = 2, eDeposit = 3, eCheckBalance = 4, eExit = 5};
 
 
-const string ClientsFileName = "Clients.txt";
+const string ClientsFileName = "local db.text";
 sClient CurrentClient;
 
 void ShowMainMenue();
@@ -29,3 +29,83 @@ void Login();
 void ShowQuickWithdrawScreen();
 void ShowNormalWithDrawScreen();
 void ShowDepositScreen();
+
+vector<string> SplitString(string s, string delmi) {
+    vector<string> res;
+    short pos = 0;
+    string w;
+    while ((pos = s.find(delmi)) != string::npos)
+    {
+        w = s.substr(0, pos);
+        if (w != "") res.push_back(w);
+        s.erase(0, pos + delmi.length());
+    }
+
+    if (s != "") res.push_back(s);
+    return res;
+}
+
+sClient ConvertLinetoRecord(string line, string delmi = "#//#") {
+    vector<string> data = SplitString(line, delmi);
+
+    sClient c;
+    c.AccountNumber = data.at(0);
+    c.PinCode = data.at(1);
+    c.Name = data.at(2);
+    c.Phone = data.at(3);
+    c.AccountBalance = stoi(data.at(3));
+
+    return c; 
+}
+
+string ConvertRecordToLine(sClient Client, string Seperator = "#//#") {
+    string stClientRecord = "";
+
+    stClientRecord += Client.AccountNumber + Seperator;
+    stClientRecord += Client.PinCode + Seperator;
+    stClientRecord += Client.Name + Seperator;
+    stClientRecord += Client.Phone + Seperator;
+    stClientRecord += to_string(Client.AccountBalance);
+
+    return stClientRecord;
+}
+
+// vector of clients From file 
+vector<sClient>LoadCleintsDataFromFile(string FileName) {
+
+    vector< sClient> load; 
+
+    fstream write;
+    write.open(FileName,ios::in);// overwriting
+
+    if (write.is_open()) 
+    {
+        string line;
+        sClient c;
+        while (getline(write, line)) {
+            c = ConvertLinetoRecord(line);
+            load.push_back(c);
+        }
+        write.close();
+    }
+
+    return load;
+}
+
+bool FindClientByAccountNumberAndPinCode(string acc, string pin, sClient& client) {
+    vector<sClient> load = LoadCleintsDataFromFile("ClientsFileName");
+
+    for (sClient& c : load) {
+        if (c.AccountNumber == acc && c.PinCode == pin) {
+            client = c;
+            return true;
+        }
+    }
+    return false; 
+}
+
+vector<sClient> SaveCleintsDataToFile(string FileName, vector <sClient> &vClients) {
+
+    fstream write;
+    write.open(FileName, ios::out); // overwrite 
+}
